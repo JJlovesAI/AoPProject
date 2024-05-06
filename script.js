506aeb1b71bb55bc45f9c4e5744093cb2d0d9183
JJlovesAI <64279709+JJlovesAI@users.noreply.github.com>
2024-05-05 22:33:04 -0400

    This is just the beginning...

---
+++ script.js
@@ -0,0 +1,29 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button and checkbox
+var btn = document.getElementById("agreeBtn");
+var checkbox = document.getElementById("agreeCheckbox");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the page loads, show the modal
+window.onload = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on the checkbox, enable/disable the button accordingly
+checkbox.onclick = function() {
+  btn.disabled = !checkbox.checked;
+}
+
+// When the user clicks on the agree button, close the modal
+btn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
